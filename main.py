51bf47b4111e8ee1596a3416084cb8ec511039bb
justnava04 <justnava04@gmail.com>
2024-08-21 14:37:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import streamlit as st
 import seaborn as sns
 df = sns.load_dataset('iris')
 df
+
+st.sidebar.title('Classifiers')
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'DT', 'RF', 'NN'))
