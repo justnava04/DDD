286919c0ed6dc3e7dcd01368fe8eadc84079ba1d
justnava04 <justnava04@gmail.com>
2024-08-21 14:48:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 
 df = sns.load_dataset('iris')
 df
@@ -12,17 +11,19 @@ df
 x = df.iloc[:, :-1]
 y = df['species']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)\
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 st.sidebar.title('Classifiers')
 classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
-if classifier == 'KNN'
+if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
   knn.fit(x_train, y_train)
   y_pred = knn.predict(x_test)
-  accuracy_score(y_test, y_pred)
-if classifier == 'SVM'  
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'SVM':
   svm = SVC()
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
